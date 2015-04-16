45c47cb0b55eb5776038743943616a511c55d388
Nino Samac <nino.samac@gmail.com>
2015-04-16 13:42:47 +0200

    Added test data.

---
+++ Vinoigitare/test/com/vinoigitare/services/TestSongServiceData.java
@@ -0,0 +1,215 @@
+package com.vinoigitare.services;
+
+import com.vinoigitare.model.Artist;
+import com.vinoigitare.model.Song;
+
+public class TestSongServiceData {
+	
+	public void populateTestSongs(){
+		
+	}
+
+	public Song getDalmacijaUMomOku(){
+		Song song = new Song();
+		
+		Artist misoKovac = new Artist("Mišo Kovaè");
+		song.setArtist(misoKovac );
+		song.setTitle("Dalmacija u mom oku");
+		song.setChords("G                                      D7\r\n" + 
+				"Pitaju me za{to pjevam o tom moru buri jugu,\r\n" + 
+				"D7                                     G\r\n" + 
+				"govore mi kako sanjam obecanu zemlju drugu.\r\n" + 
+				"G                              E7              a\r\n" + 
+				"Govore mi da se trgnem, dok je vrijeme, dok se moze,\r\n" + 
+				"       C           G        D7           G\r\n" + 
+				"k'o da mogu isprat kise onu davnu sol sa koze.\r\n" + 
+				"\r\n" + 
+				"   G G7    C\r\n" + 
+				"     Vidim jata onih istih ptica\r\n" + 
+				"     G\r\n" + 
+				"     istu zvijezdu previsoku...\r\n" + 
+				"          D7\r\n" + 
+				"     opet zivi stotinama lica\r\n" + 
+				"     C         D7    G\r\n" + 
+				"    Dalmacija u mom oku.\r\n" + 
+				"\r\n" + 
+				"     Znam da nije bila samo moja\r\n" + 
+				"     mozda sam u snu duboku...\r\n" + 
+				"     ali zivi stotinama boja\r\n" + 
+				"     Dalmacija u mom oku.\r\n" + 
+				"\r\n" + 
+				"Govore mi cemu price, ljudi drugi zivot zive.\r\n" + 
+				"Odvezane sve su barke i brodovi s moje rive.\r\n" + 
+				"Ali negdje zvona zvone i skidaju ljudi kape\r\n" + 
+				"i dok djecji plac se cuje sastaju se nove klape.");
+		
+		return song;
+	}
+	
+	public Song getSamoJedanDanZivota(){
+		Song song = new Song();
+		
+		Artist misoKovac = new Artist("Mišo Kovaè");
+		song.setArtist(misoKovac );
+		song.setTitle("Samo jedan dan života");
+		song.setChords("G                 D\r\n" + 
+				"SAMO JEDAN DAN ŽIVOTA\r\n" + 
+				"          G             C\r\n" + 
+				"JOŠ JEDNU ÈAŠU MI DAJTE SAD\r\n" + 
+				"       G\r\n" + 
+				"JER JA SUTRA NISAM S VAMA\r\n" + 
+				"          D        G\r\n" + 
+				"JER SUTRA UMRIJEÆU MLAD\r\n" + 
+				"           D               G\r\n" + 
+				"       MOJ ŽIVOT JE KRATAK BIO\r\n" + 
+				"          D           G\r\n" + 
+				"       I PROLAZAN KAO SAN\r\n" + 
+				"            C          G\r\n" + 
+				"       A JA ŽELIM SAMO JEDNO\r\n" + 
+				"                D         G\r\n" + 
+				"       DA ŽIVIM JOŠ JEDAN DAN\r\n" + 
+				"           D               G\r\n" + 
+				"       MOJ ŽIVOT JE KRATAK BIO\r\n" + 
+				"          D           G\r\n" + 
+				"       I PROLAZAN KAO SAN\r\n" + 
+				"            C          G\r\n" + 
+				"       A JA ŽELIM SAMO JEDNO\r\n" + 
+				"                D         G\r\n" + 
+				"       DA ŽIVIM JOŠ JEDAN DAN\r\n" + 
+				"\r\n" + 
+				"NEKA STARO DRUŠTVO DOÐE \r\n" + 
+				"K*O NEKAD DAVNO U KRAJU MOM\r\n" + 
+				"NEK SE ÈUJE STARA PJESMA\r\n" + 
+				"TAJ ZADNJI GLAS SRCA MOG\r\n" + 
+				"\r\n" + 
+				"SAMO BUKET BIJELIH RUŽA\r\n" + 
+				"NEK BUDE VJEÈNO NA GROBU MOM\r\n" + 
+				"SAMO VJETAR NEKA PRIÈA\r\n" + 
+				"TU TAJNU ŽIVOTA MOG");
+		
+		return song;
+	}
+	
+	public Song getSviPjevajuJaNeÈujem(){
+		Song song = new Song();
+		
+		Artist misoKovac = new Artist("Mišo Kovaè");
+		song.setArtist(misoKovac );
+		song.setTitle("Svi pjevaju, ja ne èujem");
+		song.setChords("   D        A   D   \r\n" + 
+				"Sad kad živim u tuðini\r\n" + 
+				"                       A\r\n" + 
+				"moje srce samo vene\r\n" + 
+				"\r\n" + 
+				"pa mi doðe da te pitam\r\n" + 
+				"       G    A        D \r\n" + 
+				"da li slièi bar na mene.\r\n" + 
+				"\r\n" + 
+				"         D      A G A    D\r\n" + 
+				"ref: Svi pjevaju ja neèujem\r\n" + 
+				"       A\r\n" + 
+				"       nikome se ne radujem\r\n" + 
+				"                     G   D\r\n" + 
+				"       samo tebe oèekujem.\r\n" + 
+				"\r\n" + 
+				"Znam da grešnik ja sam bio\r\n" + 
+				"sad mi sude uspomene\r\n" + 
+				"oprosti, odgovori\r\n" + 
+				"da li slièi bar na mene.");
+		
+		return song;
+	}
+	
+	public Song getItsAKindOfMagic(){
+		Song song = new Song();
+		
+		Artist artist = new Artist("QUEEN");
+		song.setArtist(artist);
+		song.setTitle("It's a kind of magic");
+		song.setChords("A\r\n" + 
+				"It’s a kind of magic, it’s a kind of magic, a kind of magic\r\n" + 
+				"\r\n" + 
+				"A\r\n" + 
+				"One dream one soul\r\n" + 
+				"B7/C#\r\n" + 
+				"One prize one goal\r\n" + 
+				"Dmaj7\r\n" + 
+				"One golden glance\r\n" + 
+				"A\r\n" + 
+				"Of what should be\r\n" + 
+				"\r\n" + 
+				"A\r\n" + 
+				"One shaft of light\r\n" + 
+				"B7/C#\r\n" + 
+				"That shows the way\r\n" + 
+				"Dmaj7\r\n" + 
+				"No mortal man\r\n" + 
+				"A\r\n" + 
+				"Can win this day\r\n" + 
+				"\r\n" + 
+				"A\r\n" + 
+				"The bell that rings\r\n" + 
+				"B7/C#\r\n" + 
+				"Inside your mind\r\n" + 
+				"Dmaj7\r\n" + 
+				"Is challenging\r\n" + 
+				"A\r\n" + 
+				"The doors of time\r\n" + 
+				"\r\n" + 
+				"F#m7               D\r\n" + 
+				"The waiting seems eternity\r\n" + 
+				"F#m7                 D\r\n" + 
+				"The day will dawn on sanity\r\n" + 
+				"D                 A\r\n" + 
+				"Is this a kind of magic\r\n" + 
+				"D                  A\r\n" + 
+				"There can be only one\r\n" + 
+				"E              G        D                          E\r\n" + 
+				"This rage that lasts a thousand years will soon be gone\r\n" + 
+				"\r\n" + 
+				"A                     B7/C#\r\n" + 
+				"This flame that burns inside of me\r\n" + 
+				"Dmaj7              A\r\n" + 
+				"I’m hearing secret harmonies\r\n" + 
+				"A                   B7/C#\r\n" + 
+				"The bell that rings inside your mind\r\n" + 
+				"Dmaj7              A             D          \r\n" + 
+				"Is challenging the doors of time\r\n" + 
+				"\r\n" + 
+				"\r\n" + 
+				"\r\n" + 
+				"A                   D\r\n" + 
+				"It’s a kind of magic\r\n" + 
+				"A                   D\r\n" + 
+				"It’s a kind of magic\r\n" + 
+				"\r\n" + 
+				"A      D     A\r\n" + 
+				"\r\n" + 
+				"E              G       D\r\n" + 
+				"This rage that lasts a thousand years\r\n" + 
+				"E11                         E\r\n" + 
+				"Will soon be, will soon be done\r\n" + 
+				"     D            A\r\n" + 
+				"This is a kind of magic\r\n" + 
+				"     D            A\r\n" + 
+				"There can be only one\r\n" + 
+				"     E         G       D\r\n" + 
+				"This rage that lasts a thousand years\r\n" + 
+				"             E11   E7   D\r\n" + 
+				"Will soon be done\r\n" + 
+				"\r\n" + 
+				"A                  D\r\n" + 
+				"It’s a kind of magic\r\n" + 
+				"A                  D\r\n" + 
+				"It’s a kind of magic\r\n" + 
+				"A                  D\r\n" + 
+				"It’s a kind of magic\r\n" + 
+				"A                  D\r\n" + 
+				"It’s a kind of magic\r\n" + 
+				"\r\n" + 
+				"\r\n" + 
+				"End on A");
+		
+		return song;
+	}
+}
