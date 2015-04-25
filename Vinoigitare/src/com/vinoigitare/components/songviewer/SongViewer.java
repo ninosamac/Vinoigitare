2d44b1382ab288c45ae649efea7d2cba0a96164f
Nino Samac <nino.samac@gmail.com>
2015-04-25 12:51:06 +0200

    Added capability to store a Song.

--- Vinoigitare/src/com/vinoigitare/components/songviewer/SongViewer.java
+++ Vinoigitare/src/com/vinoigitare/components/songviewer/SongViewer.java
@@ -1,16 +1,11 @@
 package com.vinoigitare.components.songviewer;
 
-import com.vaadin.shared.ui.label.ContentMode;
-import com.vaadin.ui.Label;
 import com.vaadin.ui.Panel;
 import com.vaadin.ui.VerticalLayout;
 import com.vinoigitare.model.Song;
 
 public class SongViewer extends Panel {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 7128198025964496737L;
 	private Song song;
 
