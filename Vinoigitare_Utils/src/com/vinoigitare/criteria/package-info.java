8e1bc0f038d04e797fcf15122288b3da3e7f5d5b
Nino Samac <nino.samac@gmail.com>
2015-05-21 22:30:43 +0200

    Added coments to package-info.java.

--- Vinoigitare_Utils/src/com/vinoigitare/criteria/package-info.java
+++ Vinoigitare_Utils/src/com/vinoigitare/criteria/package-info.java
@@ -1,8 +1,8 @@
 /**
- * 
- */
-/**
- * @author nino.samac
+ * Classes for building filtering logic for instances or collections of objects.
+ * See <code>Criteria</code> and <code>Filter</code> interfaces for details.
+ *  
+ * @author nino.samac 
  *
  */
 package com.vinoigitare.criteria;
