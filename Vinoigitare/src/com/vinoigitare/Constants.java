5d7a11a0f0ec1c80e821d56c5f1cf8e6ccd7d2a7
Nino Samac <nino.samac@gmail.com>
2015-05-23 18:23:08 +0200

    Using bigger icons. Moved icons to icons/24x24 folder.

--- Vinoigitare/src/com/vinoigitare/Constants.java
+++ Vinoigitare/src/com/vinoigitare/Constants.java
@@ -8,23 +8,28 @@ package com.vinoigitare;
  */
 public interface Constants {
 
+	// Folders
+	static String FOLDER_ICONS = "icons/24x24/";
+
 	// Styles
 	static String STYLE_SONG_TITLE = "song-title";
 	static String STYLE_SONG_ARTIST = "song-artist";
 	static String STYLE_SONG_CHORDS = "song-chords";
-	
+
 	static String STYLE_DIALOG_TITLE = "dialog-title";
 	static String STYLE_DIALOG_SUBTITLE = "dialog-subtitle";
 
 	static String STYLE_TOOLBAR = "toolbar";
 	static String STYLE_CONFIRMATION_BUTTONS = "confirmation-buttons";
-	
+
 	// Icons
-	static String ICON_NEW_SONG = "icons/Document2.gif";
-	static String ICON_EDIT_SONG = "icons/Write2.gif";
-	static String ICON_REMOVE_SONG = "icons/Cancel.gif";
-	static String ICON_ABOUT = "icons/Info.gif";
+	static String ICON_NEW_SONG = FOLDER_ICONS + "Document2.gif";
+	static String ICON_EDIT_SONG = FOLDER_ICONS + "Write2.gif";
+	static String ICON_REMOVE_SONG = FOLDER_ICONS + "Cancel.gif";
 	
+	static String ICON_SEARCH = FOLDER_ICONS + "Search.gif";
+	
+	static String ICON_ABOUT = FOLDER_ICONS + "Info.gif";
 	
 
 }
